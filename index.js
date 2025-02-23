448c6a632cb4e281dce865e9416b4cb56b679898
Eric Miranda <eric.miranda.ext@siemens-energy.com>
2025-02-23 16:51:30 +0530

    communicate with backend username & pass

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 import express from 'express'
+import cors from 'cors'
+import bodyParser from 'body-parser'
+
 const app = express()
+app.use(cors())
+app.use(bodyParser.json())
 const port = 3000
 
-app.get('/', () => {
-    console.log('henlo')
-}, (req, res) => {
+app.post('/', (req, res) => {
+    console.log(req.body)
     res.send('Hello there')
 })
 
