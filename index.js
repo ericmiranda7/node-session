6c372103ad0e8355de3119705d77d953e793afd8
Eric Miranda <eric.miranda.ext@siemens-energy.com>
2025-02-24 08:08:23 +0530

    add registration flow + nodemon

--- index.js
+++ index.js
@@ -1,15 +1,35 @@
 import express from 'express'
 import cors from 'cors'
 import bodyParser from 'body-parser'
+import mysql from 'mysql2/promise'
 
 const app = express()
 app.use(cors())
 app.use(bodyParser.json())
 const port = 3000
 
-app.post('/', (req, res) => {
+const conn = await mysql.createConnection('mysql://root:pass@localhost:5436/sessions')
+
+
+app.post('/login', async (req, res) => {
     console.log(req.body)
-    res.send('Hello there')
+    const [result, fields] = await conn.execute(
+        'SELECT * FROM users WHERE username = ?',
+        ['eric']
+    )
+    console.log(result)
+    res.send('Hello there' + result[0])
+})
+
+app.post('/register', async (req, res) => {
+    console.log('reg details', req.body)
+
+    await conn.execute(
+        'INSERT INTO users VALUES (?, ?)',
+        [req.body.username, req.body.password]
+    )
+
+    res.send('User registered')
 })
 
 app.listen(port, () => {
