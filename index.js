fafd2e89b81a7f3a23afd35fe8ba0c4b83e8dc8a
Eric Miranda <eric.miranda.ext@siemens-energy.com>
2025-02-24 11:57:18 +0530

    add express-session

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ import bodyParser from 'body-parser'
 import mysql from 'mysql2/promise'
 import bkfd2Password from 'pbkdf2-password'
 import cookieParser from 'cookie-parser'
+import session from 'express-session'
+import MySQLStore from 'express-mysql-session'
+
 
 const hasher = bkfd2Password()
 const app = express()
@@ -12,25 +15,43 @@ app.use(cors({
     credentials: true,
 }))
 app.use(bodyParser.json())
-app.use(cookieParser())
+// app.use(cookieParser())
 const port = 3000
 
 const conn = await mysql.createConnection('mysql://root:pass@localhost:5436/sessions')
+var mySQLStore = MySQLStore(session)
+const sessionStore = new mySQLStore({}, conn)
+app.use(session({
+  resave: false, // don't save session if unmodified
+  saveUninitialized: false, // don't create session until something stored
+  secret: 'shhhh, very secret',
+  store: sessionStore,
+  cookie: { secure: false, httpOnly: true, sameSite: 'lax' },
+}));
 
-// create sessions table
-// middleware to check if session cookie then set session
+// Optionally use onReady() to get a promise that resolves when store is ready.
+sessionStore.onReady().then(() => {
+    // MySQL session store ready for use.
+    console.log('MySQLStore ready');
+}).catch(error => {
+    // Something went wrong.
+    console.error(error);
+});
+
+// (done for us) create sessions table
+// (done for us) middleware to check if session cookie then set session
 // middleware to authenticate session on all routes except login & register
-// on login, generate session ID
-// store session ID in cookie
+// (done for us) on login, generate session ID
+// (done for us) store session ID in cookie
 
 const sessionMid = (req, res, next) => {
-    if (req.cookies.session) {
-        console.log('contains cookie')
+    if (req.session.user) {
+        console.log('contains user session')
         // todo(): get session data
         // todo(): set session data
         next()
     } else {
-        console.log('no cookie')
+        console.log('no session')
         res.status(401).send("Unauthenticated")
     }
 }
@@ -42,10 +63,17 @@ app.get('/protected', sessionMid, (req, res) => {
 
 app.post('/login', async (req, res) => {
     const [users, fields] = await conn.execute('SELECT * FROM users WHERE username = ?', [req.body.username])
-    hasher({password: req.body.password, salt: users[0].salt}, (err, pass, salt, hash) => {
+    hasher({ password: req.body.password, salt: users[0].salt }, (err, pass, salt, hash) => {
         if (hash === users[0].password) {
             console.log('yay')
-            res.cookie('session', '123', {sameSite: 'lax', secure: false, httpOnly: true}).send('cookie set')
+            req.session.regenerate(function () {
+                // Store the user's primary key
+                // in the session store to be retrieved,
+                // or in this case the entire user object
+                req.session.user = {username: users[0].username};
+                console.log(req.session)
+                res.send('login success')
+            });
         }
         else console.log('nay')
     })
